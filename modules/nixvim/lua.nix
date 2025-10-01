3368481801888e77da61d348302117ee4d36f22a
birnx <birnxy@gmail.com>
2025-10-01 01:26:46 -0700

    lkjlkjlklkjllkjkjljljljhlj

--- modules/nixvim/lua.nix
+++ modules/nixvim/lua.nix
@@ -1,6 +1,9 @@
 {
-  # Just a small boolean function to convert a boolean to a string
   extraConfigLuaPre = ''
     function bool2str(bool) return bool and "on" or "off" end
+
+    require("obsidian").setup({
+      legacy_commands = false,
+    })
   '';
 }
