69d49e9d11136b6d17e9f0c8b9234207e6c6a9ea
birnx <birnxy@gmail.com>
2025-10-01 17:18:11 -0700

    lkjl

--- modules/nixvim/plugins/obsidian/default.nix
+++ modules/nixvim/plugins/obsidian/default.nix
@@ -1,25 +1,25 @@
 {
-  plugins = {
-    obsidian = {
-      enable = false;
-      settings = {
-        workspaces = [
-          {
-            name = "jot";
-            path = "/home/birnx/dumper/jot";
-          }
-          {
-            name = "jot2";
-            path = "/home/birnx/dumper/jot2";
-          }
-        ];
-        dir = "~/dumper";
-        new_notes_location = "current_dir";
-        completion = {
-          min_chars = 2;
-          blink = true;
-        };
-      };
-    };
-  };
+  #   plugins = {
+  #     obsidian = {
+  #       enable = false;
+  #       settings = {
+  #         workspaces = [
+  #           {
+  #             name = "jot";
+  #             path = "/home/birnx/dumper/jot";
+  #           }
+  #           {
+  #             name = "jot2";
+  #             path = "/home/birnx/dumper/jot2";
+  #           }
+  #         ];
+  #         dir = "~/dumper";
+  #         new_notes_location = "current_dir";
+  #         completion = {
+  #           min_chars = 2;
+  #           blink = true;
+  #         };
+  #       };
+  #     };
+  #   };
 }
