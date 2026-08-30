6cbfd838b7efb2106c9de6c138df5deea085b860
birnx <birnxy@gmail.com>
2026-08-30 15:41:41 -0700

    lkjh

--- modules/nixvim/plugins/wakatime/default.nix
+++ modules/nixvim/plugins/wakatime/default.nix
@@ -1,8 +1,1 @@
-{ config, ... }:
-{
-  # vim-wakatime documentation
-  # See: https://github.com/wakatime/vim-wakatime
-  plugins.wakatime.enable =
-    config.khanelivim.integrations.accountBacked.enable
-    && config.khanelivim.integrations.accountBacked.timeTracking.enable;
-}
+{ plugins.wakatime.enable = false; }
